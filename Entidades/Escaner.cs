d269fd6389ce07998a68e037cb7902cc4f8f04b6
Lucio Belloni <belloniluciogabriel@gmail.com>
2024-05-16 02:10:02 -0300

    Falta terminar la clase Escaner

--- Entidades/Escaner.cs
+++ Entidades/Escaner.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,53 @@ namespace Entidades
 {
     public class Escaner
     {
+        private List<Documento> listaDocumentos;
+        private Departamento locacion;
+        private string marca;
+        private TipoDoc tipo;
 
+        public Escaner(string marca, TipoDoc tipo)
+        {
+            this.marca = marca;
+            this.listaDocumentos = new List<Documento>();
+            if (TipoDoc.mapa == tipo)
+            {
+                this.locacion = Departamento.mapoteca;
+            }
+            else
+            {
+                this.locacion = Departamento.procesosTecnicos;
+            }
+
+            
+        }
+
+        public static bool operator ==(Escaner e, Documento d)
+        {
+            foreach (Documento dc in e.listaDocumentos)
+            {
+                if (dc == d) return true;
+            }
+            return false;    
+        }
+
+        public static bool operator !=(Escaner e, Documento d)
+        {
+            foreach (Documento dc in e.listaDocumentos)
+            {
+                if (!(dc == d)) return true;
+            }
+            return false;
+        }
+
+        public static bool operator +(Escaner e, Documento d)
+        {
+
+        }
+
+        public List<Documento> ListaDocumentos { get => listaDocumentos;}
+        public Departamento Locacion { get => locacion; }
+        public string Marca { get => marca;}
+        public TipoDoc Tipo { get => tipo; }
     }
 }
---
+++ Entidades/Informes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    internal static  class Informes
+    {
+        public void MostrarDistribuido(Escaner e, out int extension, out int cantidad, out string resumen);
+
+    }
+}
